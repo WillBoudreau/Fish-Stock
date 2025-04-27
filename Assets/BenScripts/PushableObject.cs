d1b779fd5faeeea458b76e57c410c545248cc4f6
BenjaminLM95 <benjamin.lerner95@gmail.com>
2025-04-27 18:08:15 -0300

    Not reference in pushable blocks issue fixed. Player can jump in pushable and punchable blocks

--- Assets/BenScripts/PushableObject.cs
+++ Assets/BenScripts/PushableObject.cs
@@ -7,19 +7,69 @@ public class PushableObject : MonoBehaviour
     private Rigidbody2D pushBody;
     public bool isPush;
     public PlayerController playerController;
+    public GameObject playerOne; 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        pushBody = GetComponent<Rigidbody2D>();
-        playerController = GameObject.FindObjectOfType<PlayerController>();        
+        pushBody = GetComponent<Rigidbody2D>();             
         pushBody.bodyType = RigidbodyType2D.Static; 
+    }
+
+    void Awake() 
+    {
+        /*
+        playerOne = GameObject.Find("Player");
+
+        if (playerOne != null) 
+        {
+            playerController = playerOne.GetComponent<PlayerController>(); 
+
+            if(playerController != null) 
+            {
+                Debug.Log("Script Found");
+            }
+            else 
+            {
+                Debug.Log("There is no player Controller"); 
+            }
+        }
+        else 
+        {
+            Debug.Log("Player not found"); 
+        }
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(playerOne == null) 
+        {
+            playerOne = GameObject.Find("Player");
+
+            if (playerOne != null)
+            {
+                playerController = playerOne.GetComponent<PlayerController>();
+
+                if (playerController != null)
+                {
+                    Debug.Log("Script Found");
+                }
+                else
+                {
+                    Debug.Log("There is no player Controller");
+                }
+            }
+            else
+            {
+                Debug.Log("Player not found");
+            }
+        }
+
+
+
         if (isPush)
         {
             pushBody.velocity = new Vector2(playerController.body.velocity.x , pushBody.velocity.y);
